5f82fc4617c2a99bf24430124b8c6a36f44045ca
Lina <109418061+linamarieotte@users.noreply.github.com>
2025-03-28 11:57:34 +0100

    adds startupbean

---
+++ demo/src/main/java/com/example/demo/StartupBean.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.openapitools.client.api.DefaultApi;
+import org.openapitools.client.model.GameDto;
+import org.openapitools.client.model.GameInputDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupBean {
+    @PostConstruct
+    public void init() {
+        DefaultApi defaultApi = new DefaultApi();
+
+        GameInputDto gameInput = new GameInputDto();
+        gameInput.setGroupName("test");
+
+        GameDto result = defaultApi.gamePost(gameInput);
+
+        System.out.println(result);
+    }
+}
